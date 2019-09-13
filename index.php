3d02d2294e70e819c3b9b868cb5bb78f5f882fdd
franz8400 <55277048+franz8400@users.noreply.github.com>
2019-09-13 14:46:02 +0200

    Mobile

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php header('Location: /public_html/index.html'); ?>
+ <?php
+
+if( stristr( $_SERVER['HTTP_USER_AGENT'],'iphone' ) || 
+        stristr( $_SERVER['HTTP_USER_AGENT'],'ipod' ) || 
+        stristr( $_SERVER['HTTP_USER_AGENT'],'ipad' ))
+        {
+        header('Location: /public_html_mobile/index.html');
+    }
+    else
+    {
+        header('Location: /public_html/index.html');
+    }
+    
+?> 
